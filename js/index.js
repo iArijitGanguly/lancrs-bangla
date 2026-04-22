d8d42d34aeafb468c364d30f00aee6a3797d3a6b
Arijit Ganguly <aganguly158@gmail.com>
2026-04-22 21:11:35 +0530

    populate category function is now in action

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
 import { productsByCategory } from './products.js';
+
+function populateCategories() {
+    const categoryList = document.getElementById('categoryList');
+    const categories = Object.keys(productsByCategory);
+    
+    categories.forEach((category) => {
+        const categoryItem = document.createElement('a');
+
+        categoryItem.className = 'category-item';
+
+        categoryItem.textContent = category;
+
+        categoryList.appendChild(categoryItem);
+    });
+}
+
+populateCategories();
